fbcc8ee1a30fa3d6639aa8e0c6542590a75909e3
livolleyball <298067772@qq.com>
2017-09-14 10:15:34 +0800

    city_menu_tree

---
+++ city_menu_tree/menu_tree.py
@@ -0,0 +1,10 @@
+menu_list =[]
+
+for t in ["YELLOW", "WHITE", "BLUE", "GREEN", "RED", "BLACK", "BROWN", "PURPLE", "ORANGE"]:
+
+    menu = t
+    menu_list.append(menu)
+
+
+print menu
+print menu_list
