9cef493a8dea015581c70522cf48005d4b163c78
livolleyball <11825698+livolleyball@users.noreply.github.com>
2018-10-04 07:34:25 +0800

    pattern.findall replace pattern.match

--- python_test/sql_analyze.py
+++ python_test/sql_analyze.py
@@ -1,10 +1,11 @@
 import string
 import re
 import sys
+
 result = set()
 
-sql_file = sys.argv[1]          #脚本的路径
-result_tb_name= sys.argv[2]    # 目标表
+sql_file = sys.argv[1]  # 脚本的路径
+result_tb_name = sys.argv[2]  # 目标表
 
 # 将正则表达式编译成Pattern对象
 pattern = re.compile(r'(ads|bi|dim|dm_risk|dm_tmp|dm_ugc|dmp|dw|dwa|dwd|dws|ods|push_data|rpt|upf)(\.)([a-zA-Z0-9_]+)')
@@ -13,23 +14,23 @@ try:
     openFile = open(sql_file, 'r', encoding='UTF-8')
 
     for line in openFile.readlines():
-        # print(line)
-        match = pattern.search(line)
+        match = pattern.findall(line)
         if match:
-            match_result=match.group()
-            result.add(match_result)
+            print(match)
+            match_result=set(list(map(lambda x: "".join(x), match))) 
+            result = result | (match_result)
 
     result.remove(result_tb_name)
 except KeyError:
     print("Error: 目标表名有误,请检查")
 else:
-    print("目标表：",result_tb_name)
+    print("目标表：", result_tb_name)
     print("-------------------------------------")
     print("依赖表：共 %d 个" % len(result))
-    result_list=list(result)
+    result_list = list(result)
     result_list.sort()
-    for index,value in enumerate(result_list):
-        print(index+1,value)
+    for index, value in enumerate(result_list):
+        print(index + 1, value)
 
 finally:
     openFile.close()
